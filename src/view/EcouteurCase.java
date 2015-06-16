a5cf7d01b3a0d4c8ef78e5771a139b166f786581
VincentFougeras <fougeras.vincent@gmail.com>
2015-06-16 10:57:38 +0200

    Added cases and corrected bug with Aide.

---
+++ src/view/EcouteurCase.java
@@ -0,0 +1,13 @@
+package view;
+
+import java.awt.event.*;
+
+public class EcouteurCase implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		
+
+	}
+
+}
--- src/view/FenetrePrincipale.java
+++ src/view/FenetrePrincipale.java
@@ -11,16 +11,22 @@ import controller.Partie;
 public class FenetrePrincipale extends JFrame {
 	
 	private Partie partie;
-	private ChoixCouleur choixCouleur;
-	private ChoixImage choixImage;
+	//private ChoixCouleur choixCouleur;
+	//private ChoixImage choixImage;
 	private Regles regles;
+	private JButton[] cases = new JButton[16];
 
 	public FenetrePrincipale() {
 		super("Taquin");
+		try { // Set System L&F
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+		    e.getMessage();
+		}
 		partie = new Partie();
-		choixCouleur = new ChoixCouleur("Choix couleur");
-		choixImage = new ChoixImage("Choix image");
-		regles = new Regles("Règles");
+		//choixCouleur = new ChoixCouleur("Choix couleur");
+		//choixImage = new ChoixImage("Choix image");
+		regles = new Regles(this, "RÃ¨gles");
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setContentPane(contPane());
@@ -31,7 +37,25 @@ public class FenetrePrincipale extends JFrame {
 	
 	public JPanel contPane() {
 		JPanel contPane = new JPanel();
-		contPane.setPreferredSize(new Dimension(500, 500));
+		contPane.setBackground(Color.WHITE);
+		contPane.setLayout(new BorderLayout());
+		contPane.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+		
+		JPanel panneauDeJeu = new JPanel(new GridLayout(4,4));
+		panneauDeJeu.setPreferredSize(new Dimension(400, 400));
+		panneauDeJeu.setBackground(Color.BLUE);
+		
+		for(int i = 0; i < 16; i++) {
+			JButton bouton = new JButton();
+			bouton.setFont(new Font("Arial", Font.PLAIN, 20));
+			bouton.addActionListener(new EcouteurCase());
+			cases[i] = bouton;
+			panneauDeJeu.add(cases[i]);
+		}
+		
+		updateCases(partie.getGrille().getMatrice());
+		
+		contPane.add(panneauDeJeu, BorderLayout.CENTER);
 		return contPane;
 	}
 	
@@ -39,7 +63,7 @@ public class FenetrePrincipale extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		
 		JMenu menuPartie = new JMenu("Partie");
-		JMenu menuParametre = new JMenu("Paramètre");
+		JMenu menuParametre = new JMenu("ParamÃ¨tre");
 		JMenu menuAide = new JMenu("Aide");
 		
 		menuBar.add(menuPartie);
@@ -47,7 +71,7 @@ public class FenetrePrincipale extends JFrame {
 		menuBar.add(menuAide);
 		
 		JMenuItem itemNouvellePartie = new JMenuItem("Nouvelle ..");
-		JMenuItem itemArreterPartie = new JMenuItem("Arrêter ..");
+		JMenuItem itemArreterPartie = new JMenuItem("ArrÃªter ..");
 		
 		itemArreterPartie.setEnabled(false);
 		
@@ -74,25 +98,28 @@ public class FenetrePrincipale extends JFrame {
 		
 		JMenuItem itemCouleurParametre = new JMenuItem("Couleur");
 		JMenuItem itemImageParametre = new JMenuItem("Image");
+		itemImageParametre.setEnabled(false);
 		
 		itemCouleurParametre.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
-				choixCouleur.setVisible(true);
+				//choixCouleur.setVisible(true);
 	        }
 		});
 		
 		itemImageParametre.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
-				choixImage.setVisible(true);
+				//choixImage.setVisible(true);
 	        }
 		});
 		
 		menuParametre.add(itemCouleurParametre);
 		menuParametre.add(itemImageParametre);
 		
-		JMenuItem itemRegleAide = new JMenuItem("Règles");
+		JMenuItem itemRegleAide = new JMenuItem("RÃ¨gles");
 		JMenuItem itemCoupSuivantAide = new JMenuItem("Coup Suivant");
+		itemCoupSuivantAide.setEnabled(false);
 		JMenuItem itemSolutionAide = new JMenuItem("Solution");
+		itemSolutionAide.setEnabled(false);
 		
 		itemRegleAide.addActionListener(new ActionListener () {
 			public void actionPerformed(ActionEvent e) {
@@ -125,6 +152,20 @@ public class FenetrePrincipale extends JFrame {
 	
 	private void solution() {
 		
+	}
+	
+	private void updateCases(int[][] matrice) {
+		int cpt = 0;
+		for(int i = 0; i < matrice.length; i++){
+			for(int j = 0; j < matrice[i].length; j++){
+				if(matrice[i][j] == 0){
+					cases[cpt].setEnabled(false);
+				} else {
+					cases[cpt].setText(""+matrice[i][j]);
+				}
+				cpt++;
+			}
+		}
 	}
 
 	public static void main(String[] args) {
